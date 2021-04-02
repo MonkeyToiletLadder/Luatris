b4567c41e6eefd9c36aab2977f8a5f028d16ea9c
Dylan <dylan_w2014@outlook.com>
2021-04-02 15:36:12 -0700

    Matrix rot90

--- matrix.lua
+++ matrix.lua
@@ -1,3 +1,36 @@
 --[[
-
+    matrix.lua
+    author: vaxeral
+    april 1 2021
 ]]
+
+matrix = {}
+matrix.__index = matrix
+function matrix.new(_matrix)
+    return setmetatable(_matrix, matrix)
+end
+function matrix:rot90(k)
+    k = k or 1
+    k = k % 4
+	if k < 0 then k = k + 4 end
+	if k == 0 then
+		return self
+	else
+		local rows = #self[1]
+		local columns = #self
+		local output = {}
+		for j=1,rows,1 do
+			output[j] = {}
+			for i=1,columns,1 do
+				output[j][i] = 0
+			end
+		end
+		for j=1,rows,1 do
+			for i=1,columns,1 do
+				output[j][i] = self[#self - (i - 1)][j]
+			end
+		end
+		setmetatable(output, matrix)
+		return output:rot90(k - 1)
+	end
+end
